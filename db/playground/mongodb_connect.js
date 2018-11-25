d7fc50752ea4f20f12e4be70f0e1b18abc3cb62b
techieVignesh <r.vignesh149@gmail.com>
2018-11-25 14:01:38 +0530

    db learn started

---
+++ db/playground/mongodb_connect.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj)
+
+MongoClient.connect('mongodb://localhost:27017/todoApp', (err, db) => {
+  if(err){
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  db.collection('Todos').insertOne({
+    text: 'Somthing to do',
+    completed: false
+  }, (err, result) => {
+    if(err){
+      return console.log('Unable to unsert todo', err);
+    }
+    //result.ops -> stores all docs inserted
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  })
+
+  db.close();
+});
+
+
